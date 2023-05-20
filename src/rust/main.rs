1d3e7956507ed9c4d5aafca84c4846554bb4300b
whokilleddb <whokilleddb@gmail.com>
2023-05-21 01:00:06 +0530

    Updated Cargo

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -1,12 +1,9 @@
 mod pinfo;
 mod debug;
 
-fn main() {
-    let signature: u32;
-    let version: u16;
-    let impl_version: u16;
-    let success: bool;
 
+
+fn main() {
     let p_info = match pinfo::ProcInfo::pid_from_name("lsass.exe") {
         Ok(v) => v,
         Err(e) => {
